fd138b1e5eed89b3b0783b23af9bd0a63d14fe26
Daniel Pereira <131875304+dpereirajuli@users.noreply.github.com>
2025-02-19 13:33:00 -0300

    Delete js directory

--- js/FAQ.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.accordion').forEach(button => {
-    button.addEventListener('click', () => {
-        const faqItem = button.closest('.faq-item');
-        faqItem.classList.toggle('active');
-    });
-});
